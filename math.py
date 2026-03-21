d8ab12c9cb58df1e2530f889fd81e7de887e5e75
believeraking1-cyber <believeraking1@gmail.com>
2026-03-21 01:06:30 +0100

    "Add multiplication function to math.py"

--- math.py
+++ math.py
@@ -4,3 +4,8 @@ b = 5
 print("Addition:", a + b)
 print("Subtraction:", a - b)
 print("Multiplication:", a * b)
+
+def multiply(a, b):
+    return a * b
+
+print("5 × 7 =", multiply(5, 7))  # Output: 35
